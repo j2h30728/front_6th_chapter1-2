2f2f9007e4024ae8c0dff685398756037252e011
jihyeon <rachel2148072@gmail.com>
2025-07-15 02:13:46 +0900

    feat: normalizeVNode 함수 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,23 @@
 export function normalizeVNode(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "number" || typeof vNode === "string") {
+    return String(vNode);
+  }
+
+  if (typeof vNode.type === "function") {
+    return normalizeVNode(vNode.type({ children: vNode.children, ...vNode.props }));
+  }
+
+  if (typeof vNode.type === "string") {
+    return {
+      type: vNode.type,
+      props: vNode.props,
+      children: vNode.children?.map(normalizeVNode).filter(Boolean) || [],
+    };
+  }
+
   return vNode;
 }
