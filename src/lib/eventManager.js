64044ababf715b46c517c304532200aa2df88859
jihyeon <rachel2148072@gmail.com>
2025-07-15 23:03:13 +0900

    feat: eventManager 함수 구현

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,36 @@
-export function setupEventListeners(root) {}
+const EVENT_TYPES = ["click", "change", "input", "submit", "keydown", "keyup", "keypress"];
 
-export function addEvent(element, eventType, handler) {}
+const EVENT_MAP = new WeakMap();
 
-export function removeEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  EVENT_TYPES.forEach((eventType) => {
+    root.addEventListener(eventType, (event) => {
+      let target = event.target;
+      while (target && target !== root) {
+        const eventInfo = EVENT_MAP.get(target);
+        if (eventInfo && eventInfo.eventType === eventType) {
+          eventInfo.handler(event);
+          break;
+        }
+        target = target.parentNode;
+      }
+    });
+  });
+
+  return () => {
+    EVENT_MAP.forEach((event) => {
+      removeEvent(event.element, event.eventType, event.handler);
+    });
+  };
+}
+
+export function addEvent(element, eventType, handler) {
+  EVENT_MAP.set(element, { eventType, handler });
+}
+
+export function removeEvent(element, eventType, handler) {
+  const event = EVENT_MAP.get(element);
+  if (event.eventType === eventType && event.handler === handler) {
+    EVENT_MAP.delete(element);
+  }
+}
